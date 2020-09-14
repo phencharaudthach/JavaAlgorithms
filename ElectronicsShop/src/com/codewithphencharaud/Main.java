d12d0467a96f85e18a9139711168b15de86ef0c4
Phen Thach <pshannonthach@gmail.com>
2020-09-13 20:16:26 -0400

    algorithm for electronics shop

--- ElectronicsShop/src/com/codewithphencharaud/Main.java
+++ ElectronicsShop/src/com/codewithphencharaud/Main.java
@@ -1,20 +1,44 @@
 package com.codewithphencharaud;
 
-public class Main {
+import java.util.*;
 
-    public static int main(String[] args, int[] keyboards, int[] drives, int b) {
-        int max = 0;
-        for ( int i = keyboards.length -1; i >= 0; i--){
-            int sum = 0;
-            for (int j = drives.length-1; j >= 0; j--){
-                sum = keyboards[i]+drives[j];
-                if(sum > max && sum <= b)
-                    max = sum;
+class Solution {
+
+    static int getMoneySpent(int[] keyboards, int[] drives, int b) {
+        int spent = -1;
+        Arrays.sort(keyboards);
+        Arrays.sort(drives);
+
+        for (int i = keyboards.length - 1; i >= 0; i--) {
+            for (int j = drives.length - 1; j >= 0; j--) {
+                int total = keyboards[i] + drives[j];
+
+                if (total <= b) {
+                    spent = Math.max(spent, total);
+                }
             }
         }
-        if (max ==0)
-           return -1;
-        else
-            return max;
+        return spent;
     }
+
+    public static class Main {
+
+        public void main(String[] args) {
+            Scanner in = new Scanner(System.in);
+            int b = in.nextInt();
+            int n = in.nextInt();
+            int m = in.nextInt();
+            int[] keyboards = new int[n];
+            for (int keyboards_i = 0; keyboards_i < n; keyboards_i++) {
+                keyboards[keyboards_i] = in.nextInt();
+            }
+            int[] drives = new int[m];
+            for (int drives_i = 0; drives_i < m; drives_i++) {
+                drives[drives_i] = in.nextInt();
+            }
+            int moneySpent = getMoneySpent(keyboards, drives, b);
+            System.out.println(moneySpent);
+        }
+    }
+
 }
